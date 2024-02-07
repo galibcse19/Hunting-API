ae25be39d404393eba9c8f98f68cc815aab3796c
galibcse19 <mrgalib40@gmail.com>
2024-02-08 00:29:13 +0600

    add more API work

--- JS/api.js
+++ JS/api.js
@@ -1,17 +1,20 @@
- const LoadPhone = async () =>{
-    const res =await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+ const LoadPhone = async (Input) =>{
+     
+    const res =await fetch(`https://openapi.programming-hero.com/api/phones?search=${Input}`);
     const data = await res.json();
     const phones =data.data;
     displayPhones(phones);
 }
 const displayPhones = phones =>{
     const Container =document.getElementById('mainContainer');
+    Container.textContent='';
+
     phones.forEach(phone =>{
         console.log(phone);
          const IntoCon= document.createElement('div');
-         IntoCon.classList= `card w-96 bg-base-100 shadow-xl`;
+         IntoCon.classList= `card bg-amber-700  shadow-xl `;
          IntoCon.innerHTML=`
-                <figure class="px-10 pt-10">
+                <figure class="px-6 pt-6">
                     <img src="${phone.image}" alt="Shoes"
                         class="rounded-xl" />
                 </figure>
@@ -20,11 +23,19 @@ const displayPhones = phones =>{
                     <h2 class="card-title">${phone.phone_name}</h2>
                     <p>${phone.slug}</p>
                     <div class="card-actions">
-                        <button class="btn btn-primary">Buy Now</button>
+                        <button class="btn btn-active btn-secondary">Secondary</button>
                     </div>
                 </div>
          `;
          Container.appendChild(IntoCon);
      })
 }
-LoadPhone()
+
+const SrcNow = () =>{
+     const InputField =document.getElementById('inputField');
+     const InputData= InputField.value;
+     console.log(InputData);
+     LoadPhone(InputData);
+}
+
+// LoadPhone()
